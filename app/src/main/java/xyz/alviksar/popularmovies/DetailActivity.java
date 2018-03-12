2a3d66105e97ceaffe4b8f0386cb7961c940b96f
Alexey Sarafannikov <alviksar@gmail.com>
2018-03-12 23:07:22 +0300

    Add checkMovieIsFavorite method

--- app/src/main/java/xyz/alviksar/popularmovies/DetailActivity.java
+++ app/src/main/java/xyz/alviksar/popularmovies/DetailActivity.java
@@ -2,6 +2,7 @@ package xyz.alviksar.popularmovies;
 
 import android.content.ContentUris;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.databinding.DataBindingUtil;
 import android.net.Uri;
 import android.os.Bundle;
@@ -20,8 +21,10 @@ import xyz.alviksar.popularmovies.model.PopularMovie;
  */
 public class DetailActivity extends AppCompatActivity {
 
-
     private PopularMovie mMovie;
+
+    // The button to mark a movie as favorite
+    private Button mMarkButton;
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
@@ -49,10 +52,16 @@ public class DetailActivity extends AppCompatActivity {
         binding.setPagerAdapter(adapter);
         binding.slidingTabs.setupWithViewPager(binding.viewpager);
         binding.setPopularMovie(mMovie);
+        mMovie.setIsFavorite(checkMovieIsFavorite());
+        if (mMovie.getIsFavorite()) {
+            Toast.makeText(this, "It is a favorite movie", Toast.LENGTH_LONG).show();
+        }
+        //   TODO: Move to the loader
+        // showMovieState();
 
         // Setup button to mark movie as favorite
-        final Button markButton = findViewById(R.id.btn_mark);
-        markButton.setOnClickListener(new View.OnClickListener() {
+        mMarkButton = findViewById(R.id.btn_mark);
+        mMarkButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 boolean isFavorite = mMovie.getIsFavorite();
@@ -64,13 +73,7 @@ public class DetailActivity extends AppCompatActivity {
                     }
                 } finally {
                     mMovie.setIsFavorite(isFavorite);
-                    if (mMovie.getIsFavorite()) {
-                        markButton.setText(R.string.remove_from_favorite);
-                        markButton.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorGold));
-                    } else {
-                        markButton.setText(R.string.mark_as_favorite);
-                       markButton.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorWhite));
-                    }
+                    showMovieState();
                 }
             }
         });
@@ -92,45 +95,74 @@ public class DetailActivity extends AppCompatActivity {
                 PopularMoviesContract.MoviesEntry.CONTENT_URI,   // the user dictionary content URI
                 values                          // the values to insert
         );
-
+        return (newUri != null);
         // Show a toast message depending on whether or not the insertion was successful
-        if (newUri != null) {
-            // If the new content URI is null, then there was an error with insertion.
-            Toast.makeText(this, R.string.marked_message,
-                    Toast.LENGTH_SHORT).show();
-            return true;
-        } else {
-            // Otherwise, the insertion was successful and we can display a toast.
-            Toast.makeText(this, R.string.mark_failed_message,
-                    Toast.LENGTH_SHORT).show();
-            return false;
-
-        }
+//        if (newUri != null) {
+//            // If the new content URI is null, then there was an error with insertion.
+//            Toast.makeText(this, R.string.marked_message,
+//                    Toast.LENGTH_SHORT).show();
+//            return true;
+//        } else {
+//            // Otherwise, the insertion was successful and we can display a toast.
+//            Toast.makeText(this, R.string.mark_failed_message,
+//                    Toast.LENGTH_SHORT).show();
+//            return false;
+//
+//        }
     }
-    private boolean removeMovieFromFavorites() {
 
-     //   ContentValues values = new ContentValues();
-      //  values.put(PopularMoviesContract.MoviesEntry._ID, mMovie.getId());
-        int rowDeleted  = getContentResolver().delete(
+    private boolean removeMovieFromFavorites() {
+        //   ContentValues values = new ContentValues();
+        //  values.put(PopularMoviesContract.MoviesEntry._ID, mMovie.getId());
+        int rowDeleted = getContentResolver().delete(
                 ContentUris.appendId(
-                PopularMoviesContract.MoviesEntry.CONTENT_URI.buildUpon(), mMovie.getId()).build(),
+                        PopularMoviesContract.MoviesEntry.CONTENT_URI.buildUpon(), mMovie.getId()).build(),
                 null,
                 null
         );
-
-        // Show a toast message depending on whether or not the insertion was successful
-        if (rowDeleted != 0) {
-            // If the new content URI is null, then there was an error with insertion.
-            Toast.makeText(this, R.string.mark_removed_message,
-                    Toast.LENGTH_SHORT).show();
-            return true;
-        } else {
-            // Otherwise, the insertion was successful and we can display a toast.
-            Toast.makeText(this, R.string.mark_failed_message,
-                    Toast.LENGTH_SHORT).show();
-            return false;
-
-        }
+        return (rowDeleted != 0);
+//        // Show a toast message depending on whether or not the insertion was successful
+//        if (rowDeleted != 0) {
+//            // If the new content URI is null, then there was an error with insertion.
+//            Toast.makeText(this, R.string.mark_removed_message,
+//                    Toast.LENGTH_SHORT).show();
+//            return true;
+//        } else {
+//            // Otherwise, the insertion was successful and we can display a toast.
+//            Toast.makeText(this, R.string.mark_failed_message,
+//                    Toast.LENGTH_SHORT).show();
+//            return false;
+//
+//        }
     }
 
+    private boolean checkMovieIsFavorite() {
+
+        Cursor cursor = getContentResolver().query(
+                ContentUris.appendId(
+                        PopularMoviesContract.MoviesEntry.CONTENT_URI.buildUpon(), mMovie.getId()).build(),
+                null,
+                null,
+                null,
+                null);
+
+        int rows = 0;
+        if (cursor != null) {
+            rows = cursor.getCount();
+            cursor.close();
+        }
+        if (rows > 1) throw new RuntimeException("Inconsistent data.");
+        return (rows == 1);
+
+    }
+
+    private void showMovieState() {
+        if (mMovie.getIsFavorite()) {
+            mMarkButton.setText(R.string.remove_from_favorite);
+            mMarkButton.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorGold));
+        } else {
+            mMarkButton.setText(R.string.mark_as_favorite);
+            mMarkButton.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.colorWhite));
+        }
+    }
 }
